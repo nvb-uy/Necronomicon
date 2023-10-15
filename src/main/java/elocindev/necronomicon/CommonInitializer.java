80e7fb7fbc606a3d19d486573b7aa09476351a5f
nvb-uy <almhak@proton.me>
2023-10-15 19:16:49 -0300

    version(1.0.8)

--- src/main/java/elocindev/necronomicon/CommonInitializer.java
+++ src/main/java/elocindev/necronomicon/CommonInitializer.java
@@ -3,7 +3,7 @@ package elocindev.necronomicon;
 import elocindev.necronomicon.api.config.v1.NecConfigAPI;
 
 public class CommonInitializer {
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
 
     /**
      * An example on how to register your custom config class.
